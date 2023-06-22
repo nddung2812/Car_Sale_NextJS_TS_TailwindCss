6cfd8faebaf923f1576f8f9d31eeb4ef07b7c3cc
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-06-22 16:55:06 +1000

    add nav and footer

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import CustomButton from "./CustomButton";
+
+const Navbar = () => {
+  return (
+    <header className="w-full absolute">
+
+    </header>
+  )
+}
+
+export default Navbar
