58e04740bf2fd3472bac4c9964bb7d943680fba9
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-03 14:17:34 +1000

    remove stuff

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,3 @@
-// import { CarProps, FilterProps } from "@types";
-
 export const calculateCarRent = (city_mpg: number, year: number) => {
   const basePricePerDay = 50; // Base rental price per day in dollars
   const mileageFactor = 0.1; // Additional rate per mile driven
