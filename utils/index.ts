72a855156fc1539ad16408a8058e7d488ef3c48b
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-03 15:35:31 +1000

    test

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export const calculateCarRent = (city_mpg: number, year: number) => {
   const basePricePerDay = 50; // Base rental price per day in dollars
   const mileageFactor = 0.1; // Additional rate per mile driven
@@ -12,6 +14,32 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0);
 };
+
+// export const updateSearchParams = (type: string, value: string) => {
+//   // Get the current URL search params
+//   const searchParams = new URLSearchParams(window.location.search);
+
+//   // Set the specified search parameter to the given value
+//   searchParams.set(type, value);
+
+//   // Set the specified search parameter to the given value
+//   const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+//   return newPathname;
+// };
+
+// export const deleteSearchParams = (type: string) => {
+//   // Set the specified search parameter to the given value
+//   const newSearchParams = new URLSearchParams(window.location.search);
+
+//   // Delete the specified search parameter
+//   newSearchParams.delete(type.toLocaleLowerCase());
+
+//   // Construct the updated URL pathname with the deleted search parameter
+//   const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
+
+//   return newPathname;
+// };
 
 export async function fetchCars() {
 //   const { manufacturer, year, model, limit, fuel } = filters;
@@ -36,3 +64,17 @@ export async function fetchCars() {
   return result;
 }
 
+// export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+//   const url = new URL("https://cdn.imagin.studio/getimage");
+//   const { make, model, year } = car;
+
+//   url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+//   url.searchParams.append('make', make);
+//   url.searchParams.append('modelFamily', model.split(" ")[0]);
+//   url.searchParams.append('zoomType', 'fullscreen');
+//   url.searchParams.append('modelYear', `${year}`);
+//   // url.searchParams.append('zoomLevel', zoomLevel);
+//   url.searchParams.append('angle', `${angle}`);
+
+//   return `${url}`;
+// } 
