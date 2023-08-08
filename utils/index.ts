ddab633b579b5480a296b214eb3ff76850428059
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-08 10:49:20 +1000

    try with imagin studio image

--- utils/index.ts
+++ utils/index.ts
@@ -64,17 +64,17 @@ export async function fetchCars() {
   return result;
 }
 
-// export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-//   const url = new URL("https://cdn.imagin.studio/getimage");
-//   const { make, model, year } = car;
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
 
-//   url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
-//   url.searchParams.append('make', make);
-//   url.searchParams.append('modelFamily', model.split(" ")[0]);
-//   url.searchParams.append('zoomType', 'fullscreen');
-//   url.searchParams.append('modelYear', `${year}`);
-//   // url.searchParams.append('zoomLevel', zoomLevel);
-//   url.searchParams.append('angle', `${angle}`);
+  url.searchParams.append('customer', 'hrjavascript-mastery' || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(" ")[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
 
-//   return `${url}`;
-// } 
+  return `${url}`;
+} 
