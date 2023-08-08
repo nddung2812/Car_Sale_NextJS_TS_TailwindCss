06ac86b0f05174a9936d364a66ccc4878dfa371a
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-08 16:32:29 +1000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -11,46 +11,46 @@ export default async function Home({ searchParams }: HomeProps) {
     limit: searchParams.limit,
     model: searchParams.model
    });
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
-    <main className="overlow-hidden">
+    <main className='overflow-hidden'>
       <Hero />
-      <div className="mt-12 padding-x padding-y max-width" id="discover">
-        <div className="home__text-container">
-          <h1 className="text-4xl font-extrabold">
-            Car Catalogue
-          </h1>
-          <p>Explore the cars you might like</p>
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
         </div>
 
-        <div className="home__filters">
+        <div className='home__filters'>
           <SearchBar />
-          <div className="home__filter-container">
-            <CustomFilter title="fuel" options={fuels} />
-            <CustomFilter title="year" options={yearsOfProduction} />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
 
         {!isDataEmpty ? (
           <section>
-            <div className="home__cars-wrapper">
-              {allCars?.map((car) =>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
                 <CarCard car={car} />
-              )}
+              ))}
             </div>
 
             <ShowMore
-              pageNumber={(searchParams.limit || 10)/10}
+              pageNumber={(searchParams.limit || 10) / 10}
               isNext={(searchParams.limit || 10) > allCars.length}
             />
           </section>
         ) : (
-          <div className="home__error-container">
-            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
       </div>
     </main>
-  )
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import Image from "next/image";
 
 import CustomButton from "./CustomButton";
-import { MouseEvent } from "react";
 
 const Navbar = () => {
   return (
