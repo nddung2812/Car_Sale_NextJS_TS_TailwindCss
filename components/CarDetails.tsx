bb74b4800df3db47d427ea5f2fb9a089e8de9024
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-08 10:58:21 +1000

    try with imagin studio image

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -4,6 +4,7 @@ import { Fragment } from 'react';
 import { CarProps } from '@/types';
 import Image from 'next/image';
 import { Dialog, Transition } from '@headlessui/react';
+import { generateCarImageUrl } from '@/utils';
 
 interface CarDetailsProps {
     isOpen: boolean;
@@ -55,17 +56,17 @@ const CarDetails = ({isOpen, closeModal, car}:CarDetailsProps) => {
 
                 <div className="flex-1 flex flex-col gap-3">
                   <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
-                    <Image src="/hero.png" alt="car model" fill priority className='object-contain' />
+                    <Image src={generateCarImageUrl(car)} alt="car model" fill priority className='object-contain' />
                   </div>
                   <div className="flex gap-3">
                     <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                      <Image src="/hero.png" alt="car model" fill priority className='object-contain' />
+                      <Image src={generateCarImageUrl(car)} alt="car model" fill priority className='object-contain' />
                     </div>
                     <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                      <Image src="/hero.png" alt="car model" fill priority className='object-contain' />
+                      <Image src={generateCarImageUrl(car)} alt="car model" fill priority className='object-contain' />
                     </div>
                     <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                      <Image src="/hero.png" alt="car model" fill priority className='object-contain' />
+                      <Image src={generateCarImageUrl(car)} alt="car model" fill priority className='object-contain' />
                     </div>
                   </div>
                 </div>
