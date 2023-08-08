d0df91e5e95bc96534d8e089dde85b2acc6bcfb7
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-08 16:23:25 +1000

    update useRouter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState, Fragment} from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Listbox, Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
 import { updateSearchParams } from '@/utils';
