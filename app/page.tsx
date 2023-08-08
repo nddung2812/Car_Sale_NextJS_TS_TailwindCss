9b2bb92bf1e21e060bb6d57ab034500f35ea8642
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-08 15:27:54 +1000

    add show more page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
+import { Hero, SearchBar, CustomFilter, CarCard, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 import { HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
@@ -39,6 +39,11 @@ export default async function Home({ searchParams }: HomeProps) {
                 <CarCard car={car} />
               )}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10)/10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
--- types/index.ts
+++ types/index.ts
@@ -52,3 +52,8 @@ export interface CustomFilterProps {
     title: string;
     options: OptionProps[];
 }
+
+export interface ShowMoreProps {
+    isNext: boolean;
+    pageNumber: number;
+}
