0865fc9868080fbdfdf300eee3ac35c8035dffa4
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-08 16:24:21 +1000

    update handle scroll

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,11 @@ import CustomButton from './CustomButton'
 
 const Hero = () => {
     const handleScroll = () => {
+        const nextSection = document.getElementById("discover");
 
+        if (nextSection) {
+          nextSection.scrollIntoView({ behavior: "smooth" });
+        }
     }
   return (
     <div className='hero'>
