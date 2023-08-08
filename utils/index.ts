1544ed3afc6dbf9527c5c4195f1eb1e0613eeec0
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-08 12:29:31 +1000

    change model to subaru

--- utils/index.ts
+++ utils/index.ts
@@ -50,7 +50,7 @@ export async function fetchCars() {
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
 
-  const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=suzuki';
+  const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=subaru';
   const response = await fetch(
     url,
     {
