f3f4535aefec787a69df7d3f2c63a59facd0758c
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-08 15:03:42 +1000

    update search

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,14 +4,24 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { Listbox, Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
+import { updateSearchParams } from '@/utils';
 
 const CustomFilter = ({ title, options }:CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0])
+  const router = useRouter();
+  const handleUpdateParams = ( e: {title: string, value: string} ) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName)
+  }
   return (
     <div className="w-fit">
       <Listbox
         value={selected}
-        onChange={(e) => setSelected(e)}
+        onChange={(e) => {
+          setSelected(e)
+          handleUpdateParams(e)
+        }}
       >
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
--- utils/index.ts
+++ utils/index.ts
@@ -15,18 +15,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return rentalRatePerDay.toFixed(0);
 };
 
-// export const updateSearchParams = (type: string, value: string) => {
-//   // Get the current URL search params
-//   const searchParams = new URLSearchParams(window.location.search);
+export const updateSearchParams = (type: string, value: string) => {
+  // Get the current URL search params
+  const searchParams = new URLSearchParams(window.location.search);
 
-//   // Set the specified search parameter to the given value
-//   searchParams.set(type, value);
+  // Set the specified search parameter to the given value
+  searchParams.set(type, value);
 
-//   // Set the specified search parameter to the given value
-//   const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+  // Set the specified search parameter to the given value
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
-//   return newPathname;
-// };
+  return newPathname;
+};
 
 // export const deleteSearchParams = (type: string) => {
 //   // Set the specified search parameter to the given value
